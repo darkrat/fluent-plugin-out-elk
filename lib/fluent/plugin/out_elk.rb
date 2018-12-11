ed6d860c9286d9fd1702026185d853b1e57ef5ca
Горшков Илья <gorshkov@skbkontur.ru>
2018-12-11 14:19:45 +0500

    .

--- lib/fluent/plugin/out_elk.rb
+++ lib/fluent/plugin/out_elk.rb
@@ -2,125 +2,127 @@ require 'net/http'
 require 'uri'
 require 'yajl'
 require 'fluent/output'
+module Fluent
+  class ELKOutput < Fluent::Plugin::Output
 
-class ELKOutput < Fluent::Plugin::Output
-  Fluent::Plugin.register_output('elk', self)
-  
-  def configure(conf)
-    super
-  end
-
-  def initialize
-    super
-  end
-
-  def start
-    super
-  end
-
-  def shutdown
-    super
-  end
-
-  config_param :host, :string
-
-  config_param :port, :string
-  
-  config_param :index_name, :string
-
-  # Set Net::HTTP.verify_mode to `OpenSSL::SSL::VERIFY_NONE`
-  config_param :ssl_no_verify, :bool, :default => false
-
-  # Simple rate limiting: ignore any records within `rate_limit_msec`
-  # since the last one.
-  config_param :rate_limit_msec, :integer, :default => 0
-
-  # Raise errors that were rescued during HTTP requests?
-  config_param :raise_on_error, :bool, :default => true
-
-  config_param :token, :string, :default => ''
-  # Switch non-buffered/buffered plugin
-  config_param :buffered, :bool, :default => false
-
-  def set_body(req, tag, time, record)
-    req.body = Yajl.dump(data)
-    req['Content-Type'] = 'application/json'
-    req
-  end
-
-  def create_request(tag, time, record)
-    hash = {
-      a: 'logs',
-      b: @index_name
-    }
-    uri = URI::HTTP.build(host: @host, port: @port, query: hash.to_query)
-    req = Net::HTTP::Post.new(uri.request_uri)
-    set_body(req, tag, time, record)
-    return req, uri
-  end
-
-  def send_request(req, uri)
-    is_rate_limited = (@rate_limit_msec != 0 and not @last_request_time.nil?)
-    if is_rate_limited and ((Time.now.to_f - @last_request_time) * 1000.0 < @rate_limit_msec)
-      log.info('Dropped request due to rate limiting')
-      return
+    Fluent::Plugin.register_output('elk', self)
+    
+    def configure(conf)
+      super
     end
 
-    res = nil
+    def initialize
+      super
+    end
 
-    begin
-      req['authorization'] = "ELK #{@token}"
-      @last_request_time = Time.now.to_f
-      res = Net::HTTP.start(uri.host, uri.port, **http_opts(uri)) {|http| http.request(req) }
+    def start
+      super
+    end
+
+    def shutdown
+      super
+    end
+
+    config_param :host, :string
+
+    config_param :port, :string
+    
+    config_param :index_name, :string
+
+    # Set Net::HTTP.verify_mode to `OpenSSL::SSL::VERIFY_NONE`
+    config_param :ssl_no_verify, :bool, :default => false
+
+    # Simple rate limiting: ignore any records within `rate_limit_msec`
+    # since the last one.
+    config_param :rate_limit_msec, :integer, :default => 0
+
+    # Raise errors that were rescued during HTTP requests?
+    config_param :raise_on_error, :bool, :default => true
+
+    config_param :token, :string, :default => ''
+    # Switch non-buffered/buffered plugin
+    config_param :buffered, :bool, :default => false
+
+    def set_body(req, tag, time, record)
+      req.body = Yajl.dump(data)
+      req['Content-Type'] = 'application/json'
+      req
+    end
+
+    def create_request(tag, time, record)
+      hash = {
+        a: 'logs',
+        b: @index_name
+      }
+      uri = URI::HTTP.build(host: @host, port: @port, query: hash.to_query)
+      req = Net::HTTP::Post.new(uri.request_uri)
+      set_body(req, tag, time, record)
+      return req, uri
+    end
+
+    def send_request(req, uri)
+      is_rate_limited = (@rate_limit_msec != 0 and not @last_request_time.nil?)
+      if is_rate_limited and ((Time.now.to_f - @last_request_time) * 1000.0 < @rate_limit_msec)
+        log.info('Dropped request due to rate limiting')
+        return
       end
 
-    rescue => e # rescue all StandardErrors
-      # server didn't respond
-      log.warn "raises exception: #{e.class}, '#{e.message}'"
-      raise e if @raise_on_error
-    else
-       unless res and res.is_a?(Net::HTTPSuccess)
-          res_summary = if res
-                           "#{res.code} #{res.message} #{res.body}"
-                        else
-                           "res=nil"
-                        end
-          log.warn "failed to #{req.method} #{uri} (#{res_summary})"
-       end #end unless
-    end # end begin
-  end # end send_request
+      res = nil
 
-  def handle_record(tag, time, record)
-    req, uri = create_request(tag, time, record)
-    send_request(req, uri)
-  end
+      begin
+        req['authorization'] = "ELK #{@token}"
+        @last_request_time = Time.now.to_f
+        res = Net::HTTP.start(uri.host, uri.port, **http_opts(uri)) {|http| http.request(req) }
+        end
 
-  def prefer_buffered_processing
-    @buffered
-  end
+      rescue => e # rescue all StandardErrors
+        # server didn't respond
+        log.warn "raises exception: #{e.class}, '#{e.message}'"
+        raise e if @raise_on_error
+      else
+        unless res and res.is_a?(Net::HTTPSuccess)
+            res_summary = if res
+                            "#{res.code} #{res.message} #{res.body}"
+                          else
+                            "res=nil"
+                          end
+            log.warn "failed to #{req.method} #{uri} (#{res_summary})"
+        end #end unless
+      end # end begin
+    end # end send_request
 
-  def format(tag, time, record)
-    [time, record].to_msgpack
-  end
-
-  def formatted_to_msgpack_binary?
-    true
-  end
-
-  def multi_workers_ready?
-    true
-  end
-
-  def process(tag, es)
-    es.each do |time, record|
-      handle_record(tag, time, record)
+    def handle_record(tag, time, record)
+      req, uri = create_request(tag, time, record)
+      send_request(req, uri)
     end
-  end
 
-  def write(chunk)
-    tag = chunk.metadata.tag
-    chunk.msgpack_each do |time, record|
-      handle_record(tag, time, record)
+    def prefer_buffered_processing
+      @buffered
+    end
+
+    def format(tag, time, record)
+      [time, record].to_msgpack
+    end
+
+    def formatted_to_msgpack_binary?
+      true
+    end
+
+    def multi_workers_ready?
+      true
+    end
+
+    def process(tag, es)
+      es.each do |time, record|
+        handle_record(tag, time, record)
+      end
+    end
+
+    def write(chunk)
+      tag = chunk.metadata.tag
+      chunk.msgpack_each do |time, record|
+        handle_record(tag, time, record)
+    end
   end
 end
 
