e8c6c0a1bf9d5c38eb20e7498f46701067354a10
Горшков Илья <gorshkov@skbkontur.ru>
2018-12-12 16:45:19 +0500

    .

--- lib/fluent/plugin/out_elk.rb
+++ lib/fluent/plugin/out_elk.rb
@@ -67,7 +67,7 @@ module Fluent::Plugin
       index_fullname = DateTime.strptime(time.to_s,'%s').strftime(@index_name+'-%Y.%m.%d')
       uri = URI::HTTP.build({:host => @host, :port => @port, :path => '/logs/' + index_fullname})
       req = Net::HTTP::Post.new(uri.to_s)
-      log.info('uri '+ uri.to_s)
+      # log.info('uri '+ uri.to_s)
       set_body(req, tag, time, record)
       return req, uri
     end
@@ -80,7 +80,7 @@ module Fluent::Plugin
       end
 
       res = nil
-      log.info('body: '+ req.body)
+      # log.info('body: '+ req.body)
       begin
         req['authorization'] = "ELK #{@token}"
         req['Content-Type'] = 'application/json'
